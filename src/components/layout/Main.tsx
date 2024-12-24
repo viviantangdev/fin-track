bdf14870b920f511e2e9dc2a82992e8b19115962
Vivian Tang <viviantang.dev@gmail.com>
2024-12-24 07:25:29 +0100

    first commit

---
+++ src/components/layout/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Button from '../utils/Button';
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      Main
+      {/* <Button label='Add Transaction' onClick={() => console.log('hejja')}/> */}
+    </main>
+  );
+};
+
+export default Main;
